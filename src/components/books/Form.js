c3ae27380fd73ae4c029416485cae903986a2435
Ustin Vaskin <ustin.vaskin2011@gmail.com>
2019-12-05 15:13:43 +0000

    Created form and edit

--- src/components/books/Form.js
+++ src/components/books/Form.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
 
-const Form = ({ handleChange, handleSubmit, data, errors}) => {
+const Form = ({ handleChange, handleSubmit, data, errors }) => {
   return (
     <section className="section">
       <div className="container is-fluid">
         <div className="formBox">
           <form onSubmit={handleSubmit}>
             <div className="field">
-              <label className="label">Artist</label>
+              <label className="label">Author</label>
               <div className="control">
                 <input
                   className="input"
-                  name="artist"
+                  name="author"
                   placeholder="eg: The Kinks"
                   onChange={handleChange}
-                  value={data.artist || ''}
+                  value={data.author || ''}
                 />
               </div>
-              {errors.artist && <div className="help is-danger">{errors.artist}</div>}
+              {errors.author && <div className="help is-danger">{errors.author}</div>}
             </div>
             <div className="field">
               <label className="label">Title</label>
@@ -44,23 +44,18 @@ const Form = ({ handleChange, handleSubmit, data, errors}) => {
                   onChange={handleChange}
                   value={data.image || ''}
                 />
-
-
               </div>
               {errors.image && <div className="help is-danger">{errors.image}</div>}
             </div>
             <div className="field">
               <label className="label">Release Year</label>
               <div className="control">
-
                 <input
                   className="input"
                   name="releaseYear"
                   onChange={handleChange}
                   value={data.releaseYear || ''}
                 />
-
-
               </div>
               {errors.releaseYear && <div className="help is-danger">{errors.releaseYear}</div>}
             </div>
@@ -74,67 +69,34 @@ const Form = ({ handleChange, handleSubmit, data, errors}) => {
                     value={data.genre || ''}
                   >
                     <option value="" disabled>Please choose...</option>
-                    <option value="Rock">Rock</option>
-                    <option value="Jazz">Jazz</option>
-                    <option value="Hip-Hop">Hip-Hop</option>
-                    <option value="Country">Country</option>
-                    <option value="Alternative">Alternative</option>
-                    <option value="Pop">Pop</option>
-                    <option value="Electronic">Electronic</option>
-                    <option value="Reggae">Reggae</option>
-                    <option value="Folk">Folk</option>
-                    <option value="RnB">RnB</option>
-                    <option value="Metal">Metal</option>
-                    <option value="Classical">Classical</option>
+                    <option value="Rock">Fantasy</option>
+                    <option value="Jazz">Adventure</option>
+                    <option value="Hip-Hop">Romance</option>
+                    <option value="Country">Mystery</option>
+                    <option value="Alternative">Horror</option>
+                    <option value="Pop">Science Fiction</option>
+                    <option value="Electronic">Cooking</option>
+                    <option value="Reggae">Art</option>
+                    <option value="Folk">History</option>
+                    <option value="RnB">Travel</option>
+                    <option value="Metal">Motivational</option>
                   </select>
                 </div>
                 {errors.genre && <div className="help is-danger">{errors.genre}</div>}
               </div>
             </div>
             <div className="field">
-              <label className="label">Condition</label>
-              <div className="control">
-                <div className="select is-fullwidth">
-                  <select
-                    name="condition"
-                    onChange={handleChange}
-                    value={data.condition || ''}
-                  >
-                    <option value="" disabled>Please choose...</option>
-                    <option value="Mint">Mint</option>
-                    <option value="Near-Mint">Near-Mint</option>
-                    <option value="Very Good">Very Good</option>
-                    <option value="Good">Good</option>
-                    <option value="Fair">Fair</option>
-                  </select>
-                </div>
-                {errors.condition && <div className="help is-danger">{errors.condition}</div>}
-              </div>
-            </div>
-            <div className="field">
-              <label className="label">Length</label>
+              <label className="label">Pages</label>
               <div className="control">
                 <input
                   className="input"
-                  name="length"
+                  name="pg"
                   placeholder="eg: 45:33"
                   onChange={handleChange}
-                  value={data.length || ''}
+                  value={data.pg || ''}
                 />
               </div>
-              {errors.length && <div className="help is-danger">{errors.length}</div>}
-            </div>
-            <div className="field">
-              <label className="label">Notes</label>
-              <div className="control">
-                <input
-                  className="input"
-                  name="notes"
-                  onChange={handleChange}
-                  value={data.notes || ''}
-                />
-              </div>
-              {errors.notes && <div className="help is-danger">{errors.notes}</div>}
+              {errors.pg && <div className="help is-danger">{errors.pg}</div>}
             </div>
             <div className="field">
               <label className="label">Label</label>
@@ -150,86 +112,17 @@ const Form = ({ handleChange, handleSubmit, data, errors}) => {
               {errors.label && <div className="help is-danger">{errors.label}</div>}
             </div>
             <div className="field">
-              <label className="label">Size</label>
-              <div className="control">
-                <div className="select is-fullwidth">
-                  <select
-                    name="size"
-                    onChange={handleChange}
-                    value={data.size || ''}
-                  >
-                    <option value="" disabled>Please choose...</option>
-                    <option value='7'> 7 inch </option>
-                    <option value='10'>10 inch</option>
-                    <option value='12'>12 inch</option>
-                  </select>
-                </div>
-              </div>
-              {errors.size && <div className="help is-danger">{errors.size}</div>}
-            </div>
-            <div className="field">
-              <label className="label">Format</label>
-              <div className="control">
-                <div className="select is-fullwidth">
-                  <select
-                    name="format"
-                    onChange={handleChange}
-                    value={data.format || ''}
-                  >
-                    <option value="" disabled>Please choose...</option>
-                    <option value='1LP'>1LP</option>
-                    <option value='2LP'>2LP</option>
-                    <option value='3LP'>3LP</option>
-                    <option value='4LP'>4LP</option>
-                    <option value='Box Set'>Box Set</option>
-                  </select>
-                </div>
-              </div>
-              {errors.format && <div className="help is-danger">{errors.format}</div>}
-            </div>
-            <div className="field">
-              <label className="label">Speed</label>
-              <div className="control">
-                <div className="select is-fullwidth">
-                  <select
-                    name="speed"
-                    onChange={handleChange}
-                    value={data.speed || ''}
-                  >
-                    <option value="" disabled>Please choose...</option>
-                    <option value='33 1/3 RPM'>33 1/3 RPM</option>
-                    <option value='45 RPM'>45 RPM</option>
-                    <option value='78 RPM'>78 RPM</option>
-                  </select>
-                </div>
-              </div>
-              {errors.speed && <div className="help is-danger">{errors.speed}</div>}
-            </div>
-            <div className="field">
-              <label className="label">Catalogue Number</label>
+              <label className="label">ISBN</label>
               <div className="control">
                 <input
                   className="input"
-                  name="catalogueNumber"
+                  name="isbn"
                   onChange={handleChange}
-                  value={data.catalogueNumber || ''}
+                  value={data.isbn || ''}
                 />
               </div>
-              {errors.catalogueNumber && <div className="help is-danger">{errors.catalogueNumber}</div>}
+              {errors.isbn && <div className="help is-danger">{errors.isbn}</div>}
             </div>
-            <div className="field">
-              <label className="label">Barcode</label>
-              <div className="control">
-                <input
-                  className="input"
-                  name="barcode"
-                  onChange={handleChange}
-                  value={data.barcode || ''}
-                />
-              </div>
-              {errors.barcode && <div className="help is-danger">{errors.barcode}</div>}
-            </div>
-
             <button className="button is-dark">Submit</button>
           </form>
         </div>
